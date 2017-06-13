34408ba35eb46f5f1b27aa6a343d41dfb9833cad
Jonathan LOQUET <JonthanLT@users.noreply.github.com>
2017-06-13 11:23:57 +0200

    Create install.sh
    Script to install rails

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git curl zlib1g-dev subversion openssl libreadline6-dev git-core zlib1g libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf automake libtool bison
+
+curl -L get.rvm.io | bash -s stable --rails
+
+source ~/.rvm/scripts/rvm
+ruby -v
+rails -v
