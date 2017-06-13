d097ba4f749437a52bd7b469b89e36f943158f84
Jonathan LOQUET <JonthanLT@users.noreply.github.com>
2017-06-13 11:33:55 +0200

    Create install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y git curl zlib1g-dev subversion openssl libreadline6-dev git-core zlib1g libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf automake libtool bison
 
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -L get.rvm.io | bash -s stable --rails
 
 source ~/.rvm/scripts/rvm
